8cb460fa73187c34c3466f3835e87bd74a6d1ce4
McClane Howland <mcclane.howland@gmail.com>
2017-01-22 12:24:39 -0800

    trying to make csv work:

--- scraper.py
+++ scraper.py
@@ -2,26 +2,50 @@ import urllib.request
 from bs4 import BeautifulSoup
 from threading import Thread
 import time
+import csv
+
+stockList = []
+
+
 
 tickerList = ['AAPL','GOOGL']
-urlList = []
-for ticker in tickerList:
-    urlList.append('http://www.nasdaq.com/symbol/'+ticker+'/real-time')
 
-def getPrice(url):
+for ticker in tickerList:
+     stockList.append({'ticker': ticker, 'url':'http://www.nasdaq.com/symbol/'+ticker+'/real-time'})
+# for stock in stockList:
+#     print(stock['ticker'])
+#     print(stock['url'])
+# urlList = []
+# for ticker in tickerList:
+#     urlList.append('http://www.nasdaq.com/symbol/'+ticker+'/real-time')
+
+data = open('data.csv','w')
+writer = csv.writer(data)
+writer.writerow(('ticker','time','price'))
+data.close()
+
+def getPrice(ticker,url):
     while True:
         html = urllib.request.urlopen(url)
         soup = BeautifulSoup(html,'html.parser')
         tag = soup.find(id="qwidget_lastsale")
         price = tag.get_text()[1:]
+        data = open('data.csv','w')
+        writer = csv.writer(data)
+        writer.writerow((ticker,'time',str(price)))
+        data.close()
         print(price)
 
-threadList = []
 
-for url in urlList:
-    threadList.append(Thread(target = getPrice, args=(url,)))
+
+threadList = []
+for stock in stockList:
+    threadList.append(Thread(target = getPrice, args=(stock['ticker'],stock['url'])))
 for thread in threadList:
     thread.start()
+
+
+
 
 
 # t1 = Thread(target = getPrice1)
